44fca1751273cd922aa96955ed97a19925be2d41
blaiu <blaiu@163.com>
2014-06-29 22:17:41 +0800

    测试

--- Study/Base/src/main/java/cn/blaiu/base/StringTools.java
+++ Study/Base/src/main/java/cn/blaiu/base/StringTools.java
@@ -36,18 +36,6 @@ public class StringTools {
 	}
 	
 	
-	public static void main(String[] args) {
-		
-		String a = "asdksksos";
-		String b = "";
-		String c = " ";
-		String d = null;
-		
-		System.out.println(StringTools.Reverse(a));
-		System.out.println(StringTools.isEmpty(b));
-		System.out.println(StringTools.isEmpty(c));
-		System.out.println(StringTools.isEmpty(d));
-	}
 	
 	
 }

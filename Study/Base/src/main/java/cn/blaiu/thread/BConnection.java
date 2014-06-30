cadb80989fbc6fbe76986d42f869c61bbbeac9d9
blaiu <blaiu@163.com>
2014-07-01 00:57:08 +0800

    连接池修改

---
+++ Study/Base/src/main/java/cn/blaiu/thread/BConnection.java
@@ -0,0 +1,445 @@
+/**
+ * 
+ */
+package cn.blaiu.thread;
+
+import java.sql.Array;
+import java.sql.Blob;
+import java.sql.CallableStatement;
+import java.sql.Clob;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.NClob;
+import java.sql.PreparedStatement;
+import java.sql.SQLClientInfoException;
+import java.sql.SQLException;
+import java.sql.SQLWarning;
+import java.sql.SQLXML;
+import java.sql.Savepoint;
+import java.sql.Statement;
+import java.sql.Struct;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.Executor;
+
+/**
+ * @author blaiu
+ *
+ */
+public class BConnection implements Connection {
+
+	/** 开始使用此链接的时间 */
+	private long lastUsingTime = 0;
+	
+	/** jdbc连接 */
+	private Connection conn = null;
+	
+	/** 连接池 */
+	private Pool pool = null;
+	
+	public long getLastUsingTime() {
+		return lastUsingTime;
+	}
+
+	public void setLastUsingTime(long lastUsingTime) {
+		this.lastUsingTime = lastUsingTime;
+	}
+
+	public Connection getConn() {
+		return conn;
+	}
+
+	public void setConn(Connection conn) {
+		this.conn = conn;
+	}
+
+	public Pool getPool() {
+		return pool;
+	}
+
+	public void setPool(Pool pool) {
+		this.pool = pool;
+	}
+
+	@Override
+	public boolean isWrapperFor(Class<?> arg0) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public <T> T unwrap(Class<T> arg0) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void abort(Executor executor) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * 清除所有警告报告此连接的对象。调用此方法后，该方法返回null，直到一个新的警告getWarnings报道此连接的对象。
+	 */
+	@Override
+	public void clearWarnings() throws SQLException {
+		if (null != conn) {
+			conn.clearWarnings();
+		}
+		
+	}
+
+	@Override
+	public void close() throws SQLException {
+		synchronized (this) {
+			Connection c = conn;
+			conn = null;
+			if (null != c) {
+				try {
+					c.rollback();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				pool.release(c);
+			}
+		}
+	}
+	
+	/**
+	 * 关闭jdbc真实链接
+	 * @throws SQLException
+	 */
+	public void closeJdbcConnection() throws SQLException {
+		if(null != conn) {
+			conn.close();
+		}
+		conn = null;
+	}
+	
+	public void closeConnectionAndRelease() throws SQLException {
+		synchronized (this) {
+			Connection c = conn;
+			conn = null;
+			if(null != c) {
+				try {
+					c.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			pool.newConnection(c);
+		}
+	}
+
+	@Override
+	public void commit() throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Array createArrayOf(String typeName, Object[] elements)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Blob createBlob() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Clob createClob() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public NClob createNClob() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SQLXML createSQLXML() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Statement createStatement() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Statement createStatement(int resultSetType, int resultSetConcurrency)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Statement createStatement(int resultSetType,
+			int resultSetConcurrency, int resultSetHoldability)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Struct createStruct(String typeName, Object[] attributes)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean getAutoCommit() throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getCatalog() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Properties getClientInfo() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getClientInfo(String name) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getHoldability() throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public DatabaseMetaData getMetaData() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getNetworkTimeout() throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getSchema() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getTransactionIsolation() throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Map<String, Class<?>> getTypeMap() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SQLWarning getWarnings() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isClosed() throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isReadOnly() throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isValid(int timeout) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String nativeSQL(String sql) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CallableStatement prepareCall(String sql) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CallableStatement prepareCall(String sql, int resultSetType,
+			int resultSetConcurrency) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CallableStatement prepareCall(String sql, int resultSetType,
+			int resultSetConcurrency, int resultSetHoldability)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PreparedStatement prepareStatement(String sql) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PreparedStatement prepareStatement(String sql, int autoGeneratedKeys)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PreparedStatement prepareStatement(String sql, int[] columnIndexes)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PreparedStatement prepareStatement(String sql, String[] columnNames)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PreparedStatement prepareStatement(String sql, int resultSetType,
+			int resultSetConcurrency) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PreparedStatement prepareStatement(String sql, int resultSetType,
+			int resultSetConcurrency, int resultSetHoldability)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void releaseSavepoint(Savepoint savepoint) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void rollback() throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void rollback(Savepoint savepoint) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setAutoCommit(boolean autoCommit) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setCatalog(String catalog) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setClientInfo(Properties properties)
+			throws SQLClientInfoException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setClientInfo(String name, String value)
+			throws SQLClientInfoException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setHoldability(int holdability) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setNetworkTimeout(Executor executor, int milliseconds)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setReadOnly(boolean readOnly) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Savepoint setSavepoint() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Savepoint setSavepoint(String name) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setSchema(String schema) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setTransactionIsolation(int level) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setTypeMap(Map<String, Class<?>> map) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+}
--- Study/Base/src/main/java/cn/blaiu/thread/Pool.java
+++ Study/Base/src/main/java/cn/blaiu/thread/Pool.java
@@ -3,12 +3,75 @@
  */
 package cn.blaiu.thread;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 /**
  * @author blaiu
  *
  */
 public class Pool {
 
+	/** 线程队列池 */
+	private ConcurrentLinkedQueue<BConnection> pool = null;
+	
+	/** 当前使用线程池 */
+	private ConcurrentHashMap<Connection, BConnection> usingPool = null;
+	
+	private AtomNumber currentSize = new AtomNumber(0);
+	private String driver = "com.mysql.jdbc.Driver";
+	private String url = "jdbc:mysql://192.168.195.57:3306/test";
+	private String user = "root";
+	private String password = "000000";
+	
+	/** 最大连接数 */
+	private int	maxSize = 5;
+	
+	/** 最大连接数 */
+	private int minSize = 2;
+	
+	/** 初始化连接数 */
+	private int initSize = 2;
+	
+	/** 增长大小 */
+	private int increment = 2;
+	
+	/** 链接超时时间 */
+	private long timeout = 120000;
+	
+	/** 清理线程的时间间隔 */
+	private long idleTestPeriod = 120000;
+	
+	/** 是否管斌连接 */
+	private boolean closeIdleConnection = true;
+	
+	/** 线程最大空闲时间 */
+	private long maxIdleTime = 300000;
+	
+	/** 重试连接不能访问服务器 */
+	private int retryTimesWhileCanNotConnectServer = -1;
+	
+	/** 保持连接激活的SQL */
+	private String keepAliveSql = "select 1;";
+	
+	/** 最大使用时间 */
+	private long maxUsingTime = -1;
+	
+	private boolean closed = false;
+	
+	/** 清理线程 */
+	private Thread cleanPoolThread;
+	
+	/** 清理线程池标识 */
+	private boolean keepClean = true;
+	
 	class AtomNumber {
 		
 		int num = 0;
@@ -34,7 +97,7 @@ public class Pool {
 		}
 		
 		public int getValue () {
-			return num = 0;
+			return num;
 		}
 		
 		public void setValue (int n) {
@@ -42,6 +105,179 @@ public class Pool {
 				num = n;
 			}
 		}
+	}
+	
+	/**
+	 * 得到当前池中正在使用的链接数
+	 * @return
+	 */
+	public int getCurrentSize () {
+		return currentSize.getValue();
+	}
+	
+	/**
+	 * 得到当前池中正在使用的连接的数量
+	 * @return
+	 */
+	public int getCurrentUsingPoolSize(){
+		return usingPool.size();
+	}
+	
+	/**
+	 * 得到当前池中未被使用的连接的数量
+	 * @return
+	 */
+	public int getCurrentFreePoolSize(){
+		return pool.size();
+	}
+	
+	
+	
+	/**
+	 * 初始化连接池，增加池中连接，设置清理线程
+	 */
+	public void init() {
+		pool = new ConcurrentLinkedQueue<BConnection>();
+		usingPool = new ConcurrentHashMap<Connection, BConnection>();
+		closed = false;
+		currentSize.setValue(0);
+		increasePool(pool, initSize);
+		
+		//防止重复条用，造成多个清理线程
+		if (null != cleanPoolThread) {
+			keepClean = false;
+			cleanPoolThread.interrupt();	//终短当前线程
+			
+			try {
+				Thread.sleep(idleTestPeriod + 1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			keepClean = true;
+		}
+		
+		cleanPoolThread = new Thread("pool cleaner") {
+
+			@Override
+			public void run() {
+				while (keepClean) {
+					try {
+						Thread.sleep(idleTestPeriod);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					
+					//清理池中连接
+					int availableNum = pool.size();
+					BConnection conn = null;
+					
+					for (int i = 0; i < availableNum; i++) {
+						pool.poll();
+						if (null == conn) {
+							break;	//池中已无队列可用
+						}
+						
+						long now = Calendar.getInstance().getTimeInMillis();
+						
+						//空闲时间过长时关闭连接
+						if (closeIdleConnection && (conn.getLastUsingTime() + maxIdleTime < now) && currentSize.getValue() > minSize) {
+							try {
+								conn.closeJdbcConnection();
+							} catch (SQLException e) {
+								e.printStackTrace();
+							}
+							currentSize.dec(1);
+						} else {
+							//以下代码保证池中的连接可用
+							int j = 0;
+							while (j < retryTimesWhileCanNotConnectServer || retryTimesWhileCanNotConnectServer < 0) {
+								PreparedStatement pstmt;
+								try {
+									pstmt = conn.prepareStatement(keepAliveSql);
+									pstmt.execute();
+									pstmt.close();
+									break;
+								} catch (SQLException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+								DriverManager.setLoginTimeout((int)(timeout / 1000));
+								
+								try {
+									Connection jdbcConn = DriverManager.getConnection(url, user, password);
+									conn.setConn(jdbcConn);
+									break;
+								} catch (SQLException e) {
+									e.printStackTrace();
+								}
+								++j;
+							}
+							pool.add(conn);
+						}
+					}
+					
+					//清理长期为释放的连接
+					Iterator<Map.Entry<Connection, BConnection>> it = usingPool.entrySet().iterator();
+					while (it.hasNext()) {
+						BConnection bconn = it.next().getValue();
+						long now = Calendar.getInstance().getTimeInMillis();
+						if (maxUsingTime > 0 && bconn.getLastUsingTime() + maxUsingTime < now) {
+							try {
+								bconn.closeConnectionAndRelease();
+							} catch (SQLException e) {
+								e.printStackTrace();
+							}
+						}
+					}
+				}
+			}
+		};
+		cleanPoolThread.start();
+	}
+	
+	private void increasePool(ConcurrentLinkedQueue<BConnection> pool, int size) {
+		try {
+			Class.forName(driver);
+			for (int i = 0; currentSize.getValue() < maxSize && i < size; i++) {
+				Connection jdbcConn = DriverManager.getConnection(url, user, password);
+				release(jdbcConn);
+				currentSize.inc(1);
+			}
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void newConnection(Connection jdbcConn) {
+		usingPool.remove(jdbcConn);
+		BConnection conn = new BConnection();
+		conn.setPool(this);
+		DriverManager.setLoginTimeout((int)(timeout/1000));
+		try {
+			jdbcConn = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		conn.setConn(jdbcConn);
+		conn.setLastUsingTime(Calendar.getInstance().getTimeInMillis());
+		pool.add(conn);
+	}
+	
+	
+	/**
+	 * 释放连接。将jdbc放回连接池,移除已使用的链接
+	 * @param conn
+	 */
+	public void release (Connection conn) {
+		usingPool.remove(conn);
+		BConnection connection = new BConnection();
+		connection.setPool(this);
+		connection.setConn(connection);
+		connection.setLastUsingTime(Calendar.getInstance().getTimeInMillis());
+		pool.add(connection);
 	}
 	
 }
